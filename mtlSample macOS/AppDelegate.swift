f08ac04fdf9bb5e0e78b38cde1804fecc3ce6c65
gzonelee <gzonelee@gmail.com>
2022-02-21 23:00:34 +0900

    Initial Commit

---
+++ mtlSample macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  mtlSample macOS
+//
+//  Created by gzonelee on 2022/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
