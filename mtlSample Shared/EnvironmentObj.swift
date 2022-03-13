a47b1ebfe195862c2c0a2cb914a7e51c855b4769
gzonelee <gzonelee@gmail.com>
2022-03-13 12:30:17 +0900

    .

--- mtlSample Shared/EnvironmentObj.swift
+++ mtlSample Shared/EnvironmentObj.swift
@@ -93,7 +93,7 @@ class EnvironmentObj {
         renderEncoder.setDepthStencilState(depthStencilState!)
         renderEncoder.setVertexBuffer(mesh.vertexBuffers[0].buffer, offset: 0, index: 0)
         
-        var viewMatrix = uniforms.viewMatrix * matrix4x4_rotation(radians: rotation, axis: [0, 1, 0])
+        var viewMatrix = uniforms.viewMatrix * matrix4x4_rotation(radians: -rotation, axis: [0, 1, 0])
         viewMatrix.columns.3 = [0, 0, 0, 1]
         var viewProjectionMatrix = uniforms.projectionMatrix * viewMatrix
         renderEncoder.setVertexBytes(&viewProjectionMatrix, length: MemoryLayout<float4x4>.stride, index: 1)
